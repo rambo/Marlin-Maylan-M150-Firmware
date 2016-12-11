b9aaa9f12b21aee8ae8a704ef3ff30c2e21d6b43
Christoph Herndler <christoph.herndler@pool4tool.com>
2016-12-11 17:48:37 +0100

    More accurate steps per unit

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -486,7 +486,8 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {92.418075,92.418075,1616.161616,95.675436}  // default steps per unit for Ultimaker
+//#define DEFAULT_AXIS_STEPS_PER_UNIT   {92.418075,92.418075,1616.161616,95.675436}  // default steps per unit for Ultimaker
+#define DEFAULT_AXIS_STEPS_PER_UNIT {3200/34.544,3200/34.544,1600,104.00}
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -511,7 +512,7 @@
  *   M204 R    Retract Acceleration
  *   M204 T    Travel Acceleration
  */
-#define DEFAULT_ACCELERATION          3000    // X, Y, Z and E acceleration for printing moves
+#define DEFAULT_ACCELERATION          800    // X, Y, Z and E acceleration for printing moves
 #define DEFAULT_RETRACT_ACCELERATION  3000    // E acceleration for retracts
 #define DEFAULT_TRAVEL_ACCELERATION   3000    // X, Y, Z acceleration for travel (non printing) moves
 
@@ -522,8 +523,8 @@
  * When changing speed and direction, if the difference is less than the
  * value set here, it may happen instantaneously.
  */
-#define DEFAULT_XJERK                 20.0
-#define DEFAULT_YJERK                 20.0
+#define DEFAULT_XJERK                 8.0
+#define DEFAULT_YJERK                 8.0
 #define DEFAULT_ZJERK                  0.4
 #define DEFAULT_EJERK                  5.0
 
